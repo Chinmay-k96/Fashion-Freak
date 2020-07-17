b6b832e6216f81bafc4b6cb6b700da6cc6f9700a
Your Name <you@example.com>
2020-07-18 00:11:49 +0530

    Adding new logo and some css in homepage

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -9,14 +9,14 @@ import CartDropdown from '../cart-dropdown/cart-dropdown';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 
-import { ReactComponent as Logo } from '../../assets/crown.svg';
-//import Logo from '../../assets/logo.png';
+//import { ReactComponent as Logo } from '../../assets/crown.svg';
+import Logo from '../../assets/logo.png';
 import './header.scss';
 
 const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
-      <Logo className='logo' />
+      <img src={Logo} alt="Logo"  className="Logo"/>
     </Link>
     <div className='options'>
       <Link className='option' to='/shop'>
